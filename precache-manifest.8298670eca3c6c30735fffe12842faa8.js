c95251d72333551785a0f51389bcc4f64f518a1d
doomdev69 <singhavicool@gmail.com>
2020-05-26 14:11:21 +0530

    Updates

---
+++ precache-manifest.8298670eca3c6c30735fffe12842faa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09c3aedcce49a2d784e25888c395c24",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3affeed1c462165013d5",
+    "url": "/monsters-rolodex/static/css/main.ebcb8334.chunk.css"
+  },
+  {
+    "revision": "2e727d9017460540a5f7",
+    "url": "/monsters-rolodex/static/js/2.24e11aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.24e11aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3affeed1c462165013d5",
+    "url": "/monsters-rolodex/static/js/main.e5fa284f.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
